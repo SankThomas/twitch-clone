a9299bf24e92e18ae13fe3926d145ab01901ca52
Thomas Sankara <thomassankara74@gmail.com>
2021-04-05 08:40:45 +0300

    metadata

---
+++ components/Meta.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Head from "next/head"
+
+const Meta = () => {
+  return (
+    <Head>
+      <title>Twitch</title>
+      <link rel="shortcut icon" href="./favicon.png" type="image/x-icon" />
+      <meta
+        name="descrition"
+        content="Twitch is the world's leading live streaming platform for gamers and the things we love. Watch and chat now with millions of other fans from around the world."
+      />
+      <meta
+        name="keywords"
+        content="Livestreaming platform, gaming platform, watch and chat with livestreamers"
+      />
+    </Head>
+  )
+}
+
+export default Meta

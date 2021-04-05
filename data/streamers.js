12257e98377daf77e7df8089277952e75b550654
Thomas Sankara <thomassankara74@gmail.com>
2021-04-05 08:41:42 +0300

    dummy data

---
+++ data/streamers.js
@@ -0,0 +1,62 @@
+export const streamers = [
+  {
+    id: 1,
+    avatar_url:
+      "https://images.pexels.com/photos/4834891/pexels-photo-4834891.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "TsbSankara",
+  },
+  {
+    id: 2,
+    avatar_url:
+      "https://images.pexels.com/photos/1561020/pexels-photo-1561020.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "TsbSankara",
+  },
+  {
+    id: 3,
+    avatar_url:
+      "https://images.pexels.com/photos/4107337/pexels-photo-4107337.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "TsbSankara",
+  },
+  {
+    id: 4,
+    avatar_url:
+      "https://images.pexels.com/photos/1687678/pexels-photo-1687678.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "TsbSankara",
+  },
+  {
+    id: 5,
+    avatar_url:
+      "https://images.pexels.com/photos/129731/pexels-photo-129731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "TsbSankara",
+  },
+  {
+    id: 6,
+    avatar_url:
+      "https://images.pexels.com/photos/848573/pexels-photo-848573.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "TsbSankara",
+  },
+  {
+    id: 7,
+    avatar_url:
+      "https://images.pexels.com/photos/3097112/pexels-photo-3097112.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "TsbSankara",
+  },
+  {
+    id: 8,
+    avatar_url:
+      "https://images.pexels.com/photos/821718/pexels-photo-821718.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "TsbSankara",
+  },
+  {
+    id: 9,
+    avatar_url:
+      "https://images.pexels.com/photos/737586/pexels-photo-737586.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "TsbSankara",
+  },
+  {
+    id: 10,
+    avatar_url:
+      "https://images.pexels.com/photos/39561/solar-flare-sun-eruption-energy-39561.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    name: "TsbSankara",
+  },
+]
